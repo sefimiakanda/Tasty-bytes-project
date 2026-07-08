cc85fbeb38061b58c5558d4649c2b33d325bee78
fidelemiakanda <sefimiakanda@gmail.com>
2026-07-08 21:33:41 +0200

    Ajout du notebook

--- app/main.py
+++ app/main.py
@@ -1,23 +1,23 @@
+import logging
 from fastapi import FastAPI
 import joblib
 import pandas as pd
+import datetime
+
+# Configuration du log
+logging.basicConfig(filename="predictions.log", level=logging.INFO, 
+                    format="%(asctime)s - %(message)s")
 
 app = FastAPI()
-
-# Chargement du modèle au démarrage de l'API
 model = joblib.load('models/best_model.joblib')
-
-@app.get("/")
-def read_root():
-    return {"message": "Bienvenue sur l'API Tasty Bytes !"}
 
 @app.post("/predict")
 def predict(data: dict):
-    # Transformation du dictionnaire en DataFrame (format attendu par le modèle)
     df = pd.DataFrame([data])
-    
-    # Prédiction
     prediction = model.predict(df)[0]
     result = "High" if prediction == 1 else "Low"
+    
+    # Enregistrer la requête et le résultat
+    logging.info(f"Input: {data} | Prediction: {result}")
     
     return {"recipe_category": data.get('category'), "prediction": result}
